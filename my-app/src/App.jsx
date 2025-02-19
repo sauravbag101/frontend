915a5dab9f347ed9c01c58577119cde60e95a12d
sauravbag101 <sauravbag101@gmail.com>
2025-02-19 13:52:25 +0530

    tt

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Animation from './components/animation'
+
 import Home from './components/Home'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
    <>
    <Home/>
-   <Animation/>
+
    </>
   )
 }
